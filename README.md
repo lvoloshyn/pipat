056b9ef381906e31f3ad9d9f2079ff24fcbf0e48
lvoloshyn <4700700+lvoloshyn@users.noreply.github.com>
2022-08-07 15:20:20 +0300

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+## PyPi [at specific date]
+
+This tool allows you to reproduce the PyPi repository at a specific date.
+
+### Why would you need this? 
+Trying to run an old legacy code is no fun. Especially when its author is long gone, and dependencies aren't 
+provided with specific versions.
+
+Usually, it's pretty easy to find out the time the codebase was written. With this, you can use PipAT to install
+needed packages in versions that would be the latest for that specific moment.
+
+### How it works
+Let's say you're dealing with code written on 24 Feb 2016. All you need is to run PipAT, and use it as an index:
+```
+python repo.py
+pip install -i http://localhost:8000/2016-02-24/ -r requirements.txt
+pip install -i http://localhost:8000/2016-02-24/ pandas
+
+```
---
+++ repo.py
@@ -0,0 +1,75 @@
+# https://peps.python.org/pep-0503/
+
+# Actual PYPI index at: https://pypi.org/simple/pandas/
+# Additional package data at https://pypi.org/pypi/{package_name}/json
+import datetime
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+
+@dataclass
+class PythonPackageVersion:
+    filename: str
+    url: str
+    version: str
+    hash_string: str
+    requires_python: str
+    upload_time: datetime.datetime
+
+
+class PythonPackage:
+    def __init__(self, package_name: str):
+        self._package_name = package_name
+
+    def get_package_info(self):
+        url = f"https://pypi.python.org/pypi/{self._package_name}/json"
+        response = requests.get(url).json()
+        return response
+
+    def get_package_versions(self):
+        info = self.get_package_info()
+
+        result = []
+        for version, files in info["releases"].items():
+            for file_data in files:
+                filename = file_data["filename"]
+                dt = datetime.datetime.fromisoformat(file_data["upload_time"])
+
+                hash_string = "md5=%s" % file_data["md5_digest"]
+                if "sha256" in file_data["digests"]:
+                    hash_string = "sha256=%s" % file_data["digests"]["sha256"]
+
+                result.append(PythonPackageVersion(
+                    filename=filename,
+                    url=file_data["url"],
+                    version=version,
+                    hash_string=hash_string,
+                    requires_python=file_data["requires_python"],
+                    upload_time=dt
+                ))
+
+        return result
+
+    def get_package_versions_at(self, year: int, month: int, day: int):
+        dt_now = datetime.datetime(year=year, month=month, day=day, hour=0, minute=0, second=0)
+        versions_data = self.get_package_versions()
+
+        actual_versions = [item for item in versions_data if item.upload_time <= dt_now]
+        actual_versions = sorted(actual_versions, key=lambda x: x.upload_time, reverse=True)
+
+        return actual_versions
+
+
+@app.route("/<int:year>-<int:month>-<int:day>/<string:package>")
+def get_package_versions_at(year: int, month: int, day: int, package: str):
+    p = PythonPackage(package)
+    releases = p.get_package_versions_at(year=year, month=month, day=day)
+    return render_template("package.html", package=package, releases=releases)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
